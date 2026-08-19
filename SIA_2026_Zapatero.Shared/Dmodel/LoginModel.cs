8529bc7075b9b11fff890dff0661db6ab620f90c
zapateropv <paulvincentzapatero17@gmail.com>
2026-08-19 17:49:06 +0800

    added

---
+++ SIA_2026_Zapatero.Shared/Dmodel/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SIA_2026_Zapatero.Shared.Dmodel
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
