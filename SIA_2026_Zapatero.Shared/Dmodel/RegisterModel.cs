3e0392bb00c0685afddcadccb6f377b347cb4b27
zapateropv <paulvincentzapatero17@gmail.com>
2026-08-28 13:57:33 +0800

    exam

---
+++ SIA_2026_Zapatero.Shared/Dmodel/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SIA_2026_Zapatero.Shared.Dmodel
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
